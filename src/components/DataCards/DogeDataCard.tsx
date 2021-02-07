d7b2b0f9c138a7c71ae2d842a0750400f12b7b1e
Will G <will.golden@ghco.co.uk>
2021-02-07 17:34:23 -0500

    fix value update issue

--- src/components/DataCards/DogeDataCard.tsx
+++ src/components/DataCards/DogeDataCard.tsx
@@ -8,6 +8,8 @@ import DogeConfig from '../../currencyConfigs/doge'
 export default () => {
 
     let dogeConfig = new DogeConfig;
+    const [last, setLast] = useState(0);
+    const [accountValue, setaccountValue] = useState(0);
 
     const dogeTicker = async(binanceClient: any) => {
         const dogeObj = await Promise.resolve(fetchBinancePrice(dogeConfig.asset, dogeConfig.base, binanceClient));
@@ -15,23 +17,20 @@ export default () => {
         dogeConfig.setAsk(dogeObj.ask);
         dogeConfig.setBid(dogeObj.bid);
         dogeConfig.setLast(dogeObj.last);
-  
+        setLast(dogeObj.last);
         let accountValue: number= (dogeConfig.last * 6687) - 400;
         dogeConfig.setaccountValue(accountValue);
+        setaccountValue(accountValue);
   
         return dogeConfig.last;
     }
 
-    const [last, setLast] = useState(dogeConfig.last);
-    const [accountValue, setaccountValue] = useState(dogeConfig.accountValue);
-
     return(
         <div>
             <img src={logo} className="App-logo" alt="logo" />
-            <Button onClick={() => {
-            setLast(dogeConfig.last)
-            setaccountValue(dogeConfig.accountValue)
-            dogeTicker(binanceClient)
+            <Button 
+            onClick= {() => {
+                dogeTicker(binanceClient)
             }}>DOGE ME</Button>
             <h3>{last}</h3>
             <h3>{accountValue}</h3>
